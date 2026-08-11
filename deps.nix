f8e1d15bb33ca2835b9ac05d06af2f771f12e26a
paco9720 <62359183+Paco9720@users.noreply.github.com>
2026-08-10 21:35:04 -0600

    Add files via upload

--- deps.nix
+++ deps.nix
@@ -1,21 +1,32 @@
 { pkgs, ... }:
 {
-# dependences
-environment.systemPackages = with 
-pkgs; [
-  gcc
-  gnumake
-  pkg-config
-  tree-sitter
-  nodejs
-  git
-  cmake
-  ninja
-  gdb
-  ripgrep
-  fd
-  python3
-  qt6.qtbase
-  python3
-];
+  environment.systemPackages = with pkgs; [
+    gcc
+    gnumake
+    pkg-config
+    tree-sitter
+    nodejs
+    git
+    cmake
+    ninja
+    gdb
+    ripgrep
+    fd
+
+    qt6.qtbase
+
+    python3
+    mesa
+    fontconfig
+
+    zlib
+    libx11
+    harfbuzz
+    freetype
+    libxcrypt
+    gmp
+    krb5
+
+    extract-xiso
+  ];
 }
